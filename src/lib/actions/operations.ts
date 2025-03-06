e59367ccba5eeea098065f7d2632ff470b636f0f
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2025-03-06 23:17:57 +0530

    feat(operations): Add operations management server actions using Prisma

---
+++ src/lib/actions/operations.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { Operation } from "@prisma/client";
+
+import { revalidatePath } from "next/cache";
+
+export async function getOperations(
+  take: number = 0,
+  skip: number = 0,
+): Promise<Operation[]> {
+  return await prisma.operation.findMany({
+    take: take > 0 ? take : undefined,
+    skip: skip > 0 ? skip : undefined,
+  });
+}
+
+export async function getOperation(id: string): Promise<Operation | null> {
+  return await prisma.operation.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
+
+export async function createOperation(data: {
+  name: string;
+  costClinic: number;
+  costBill: number;
+  duration: number;
+}): Promise<void> {
+  await prisma.operation.create({
+    data,
+  });
+  revalidatePath("/appointments");
+}
